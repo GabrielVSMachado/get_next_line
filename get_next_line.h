2c2f77bf33b6be29631ffea240c50e430a58da45
GabrielVSM <gabrielvitorsales@gmail.com>
2021-06-10 22:38:34 -0300

    feat: add enum and function ft_calloc

--- get_next_line.h
+++ get_next_line.h
@@ -1,6 +1,11 @@
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
-# define TRUE 1
+enum returns
+{
+	gnl_ERROR = -1,
+	gnl_EOF = 0,
+	gnl_READ = 1,
+}; 
 # include <stdlib.h>
 # include <unistd.h>
 
@@ -8,7 +13,7 @@ int		get_next_line(int fd, char **line);
 size_t	ft_strlen(const char *string);
 char	*ft_strjoin(const char *s1, const char *s2);
 char	*ft_substr(char const *s, unsigned int start, size_t len);
-void	clean_buffer(char **buffer);
 char	*ft_strdup(const char *src);
+void	*ft_calloc(size_t ntype, size_t size);
 
 #endif
