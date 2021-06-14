3571d1a19377a7c61f2c0b4e319afa449d410e7a
GabrielVSM <gabrielvitorsales@gmail.com>
2021-06-14 14:38:15 -0300

    feat: add enum

--- get_next_line.h
+++ get_next_line.h
@@ -1,13 +1,13 @@
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
-enum returns
+# include <stdlib.h>
+# include <unistd.h>
+enum e_returns
 {
 	gnl_ERROR = -1,
 	gnl_EOF = 0,
 	gnl_READ = 1,
-}; 
-# include <stdlib.h>
-# include <unistd.h>
+};
 
 int		get_next_line(int fd, char **line);
 size_t	ft_strlen(const char *string);
