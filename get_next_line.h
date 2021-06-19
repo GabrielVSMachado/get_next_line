7e6b829650ce6f283048b93457b63d5b361704ff
GabrielVSM <gabrielvitorsales@gmail.com>
2021-06-19 16:51:33 -0300

    feat: add ft_strdup and remove ft_calloc

--- get_next_line.h
+++ get_next_line.h
@@ -2,6 +2,7 @@
 # define GET_NEXT_LINE_H
 # include <stdlib.h>
 # include <unistd.h>
+# define NO_ERRORS 1
 enum e_returns
 {
 	gnl_ERROR = -1,
@@ -13,8 +14,7 @@ int		get_next_line(int fd, char **line);
 size_t	ft_strlen(const char *string);
 char	*ft_strjoin(const char *s1, const char *s2);
 char	*ft_substr(char const *s, unsigned int start, size_t len);
-char	*ft_strdup(const char *src);
-void	*ft_calloc(size_t ntype, size_t size);
 char	*ft_strchr(const char *str, int c);
+char	*ft_strdup(const char *src);
 
 #endif
